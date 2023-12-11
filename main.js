3d81e58e7c2a1a327fa5405273240952d119affa
Raissa Rodrigues <raissarodrigues.ufjf@gmail.com>
2023-12-11 15:49:34 -0300

    cria funções para receber pontos

--- main.js
+++ main.js
@@ -1,35 +1,28 @@
 //main.js
 
+const jogador1 = document.getElementById('jogador1');
+const jogador2 = document.getElementById('jogador2');
+
+const dadoButton = document.getElementById('rolarDado');
+
+let jogadorPlayer = jogador1;
+let valorDoDado = 0;
+
 let pontosDoJogador1 = 0;
 let pontosDoJogador2 = 0;
 
+dadoButton.addEventListener('click', rolarDado);
 
-function calcularPontuacao(tabuleiroId) {
-    const tabuleiro = document.getElementById(tabuleiroId);
-    const cells = tabuleiro.getElementsByClassName('cell');
-    
-    let pontuacao = 0;
+function dado() {
+    valorDoDado = Math.floor (Math.random()*6)+1;
+    jogadorPlayer.getElementsByClassName('cell')[0].innerHTML = valorDoDado;
 
-    for (let coluna = 0; coluna < 3; coluna++) {
-        let produto = 1;
-        for (let linha = 0; linha < 3; linha++) {
-            const indice = coluna + linha * 3;
-            const valor = parseInt(cells[indice].textContent || 0);
-            produto *= valor;
-        }
-        pontuacao += produto;
+    const jogador2 = jogadorPlayer === jogador1 ? jogador2:jogador1;
+
+    if(jogadorPlayer === jogador1){
+        pontosDoJogador1 += calculaColunaDePontos(jogadorPlayer)
+    }else{
+        jogador2 += calculaColunaDePontos(jogadorPlayer)
     }
-
-    return pontuacao;
 }
-
-function atualizarPontosJogadores() {
-    pontosJogador1 = calcularPontuacao('tabuleiro1');
-    pontosJogador2 = calcularPontuacao('tabuleiro2');
-
-    document.getElementById('pontosJogador1').textContent = pontosJogador1;
-    document.getElementById('pontosJogador2').textContent = pontosJogador2;
-}
-
-// Exemplo de chamada de função para atualizar pontos
-atualizarPontosJogadores();
+   
